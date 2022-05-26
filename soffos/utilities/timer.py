b34b95cf7382ffbc81fe3476becae57eeea1c809
karfan <stefan.kairinos@soffos.ai>
2022-05-26 15:44:58 +0300

    get logger for timer

--- soffos/utilities/timer.py
+++ soffos/utilities/timer.py
@@ -4,17 +4,17 @@ import logging
 
 class Timer:
     def __init__(self, name: str, *name_args):
-        self.name = name % name_args
+        self.logger = logging.getLogger(f'timer.{name % name_args}')
         self.start: float = None
         self.end: float = None
         self.elapsed: float = None
 
     def __enter__(self):
-        logging.debug('%s: starting timer.', self.name)
+        self.logger.debug('Starting timer.')
         self.start = default_timer()
         return self
 
     def __exit__(self, *args):
         self.end = default_timer()
         self.elapsed = self.end - self.start
-        logging.debug('%s: %.4f seconds elapsed.', self.name, self.elapsed)
+        self.logger.debug('%.4f seconds elapsed.', self.elapsed)
