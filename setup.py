eef0784178eac3b0d59ffa2bbe65d46795c5dd4e
Ronaldo Faria Lima <ronaldo.faria.lima@gmail.com>
2022-06-02 09:47:01 -0300

    Version adjustments

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ import argparse
 # Set the version to be the 24hr, UTC+0 datetime stamp:
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
-YEAR, MONTH, DAY = 22, 5, 26  # date
-HOUR, MINUTE, SECOND = 12, 54, 34  # time
+YEAR, MONTH, DAY = 22, 6, 2  # date
+HOUR, MINUTE, SECOND = 12, 46, 0  # time
 
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-v', '--version', action='store_true', help='set version as utc now')
+arg_parser.add_argument(
+    '-v', '--version', action='store_true', help='set version as utc now')
 known_args, unknown_args = arg_parser.parse_known_args()
 
 if known_args.version:
