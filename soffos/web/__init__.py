d5021d359050994ed68db003dfb3b848c7b24f00
Ronaldo Faria Lima <ronaldo.faria.lima@soffos.ai>
2022-06-13 10:18:25 -0300

    Fix: exported Payload and Header types from web.client

--- soffos/web/__init__.py
+++ soffos/web/__init__.py
@@ -3,4 +3,4 @@ Copyright (c)2022 - Soffos.ai - All rights reserved
 Created at: 2022-06-08
 Purpose: Specialized web clients
 """
-from .client import WebClient, RetryWebClient
+from .client import WebClient, RetryWebClient, Payload, Header
