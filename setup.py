5d413e6d0493e81628d9612c6b34c1363a9efdb3
karfan <stefan.kairinos@soffos.ai>
2022-06-22 12:39:17 +0300

    bert model service request

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import argparse
 # Set the version to be the 24hr, UTC+0 datetime stamp:
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
-YEAR, MONTH, DAY = 22, 6, 20  # date
-HOUR, MINUTE, SECOND = 16, 40, 30  # time
+YEAR, MONTH, DAY = 22, 6, 22  # date
+HOUR, MINUTE, SECOND = 9, 37, 43  # time
 
 
 arg_parser = argparse.ArgumentParser()
