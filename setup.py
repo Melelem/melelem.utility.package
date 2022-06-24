9f35108ab45e351d285c7dd91dd84f48bb19aadf
karfan <stefan.kairinos@soffos.ai>
2022-06-24 12:35:25 +0300

    refactor + add ner model

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import argparse
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
 YEAR, MONTH, DAY = 22, 6, 24  # date
-HOUR, MINUTE, SECOND = 9, 14, 51  # time
+HOUR, MINUTE, SECOND = 9, 34, 35  # time
 
 
 arg_parser = argparse.ArgumentParser()
--- soffos/service/request/__init__.py
+++
@@ -1,2 +0,0 @@
-from .bert_model import BertModelService
-from .profanity_model import ProfanityModelService
