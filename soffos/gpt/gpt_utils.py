cecb0cde6e768db86b135594cc040b7193de354f
PolinaSvidov <polina.svidovsky@fountech.solutions>
2022-06-30 09:24:01 +0300

    prompt

--- soffos/gpt/gpt_utils.py
+++ soffos/gpt/gpt_utils.py
@@ -1,9 +1,8 @@
 import math
-import settings
 
 MODEL_GPT_TOKEN_CHAR_COUNT=4
 
-def calculate_token_count(data_length: int) -> int:
+def calculate_token_count_from_chars(data_length: int) -> int:
     """
     Calculates the token count from a given data. This method is based
     in english language tokens.
@@ -15,3 +14,17 @@ def calculate_token_count(data_length: int) -> int:
     - Calculated token count
     """
     return math.ceil(data_length / MODEL_GPT_TOKEN_CHAR_COUNT)
+
+
+def calculate_chars_count_from_tokens(data_length: int) -> int:
+    """
+    Calculates the char count from a given data. This method is based
+    in english language tokens.
+    Parameters
+    ----------
+    - data_length: Data length in tokens
+    Returns
+    -------
+    - Calculated chars count
+    """
+    return data_length * MODEL_GPT_TOKEN_CHAR_COUNT    
