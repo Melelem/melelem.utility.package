9335bc9ea6a16f92c0d0b5a91418675842006102
karfan <stefan.kairinos@soffos.ai>
2022-08-10 13:16:41 +0300

    support loading funcs that return none

--- soffos/utilities/lazy_loader.py
+++ soffos/utilities/lazy_loader.py
@@ -8,11 +8,13 @@ class LazyLoader(t.Generic[T]):
     def __init__(self, load: t.Callable[[], T]):
         self._load = load
         self._value: T = None
+        self._did_load = False
 
     def __call__(self):
         return self.lazy_load()
 
     def lazy_load(self):
-        if self._value is None:
+        if not self._did_load:
             self._value = self._load()
+            self._did_load = True
         return self._value
