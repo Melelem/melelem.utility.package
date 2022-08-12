65282571d5b0900e7f5190b256923258756a8d1b
karfan <stefan.kairinos@soffos.ai>
2022-08-12 13:38:53 +0300

    revert base url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import argparse
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
 YEAR, MONTH, DAY = 22, 8, 12  # date
-HOUR, MINUTE, SECOND = 9, 47, 28  # time
+HOUR, MINUTE, SECOND = 10, 38, 40  # time
 
 
 arg_parser = argparse.ArgumentParser()
