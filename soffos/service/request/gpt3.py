e4ba0a717af492c3906f3eaeff40df02a4916a3c
andrch-FS <andreas.christofi@fountech.solutions>
2022-08-18 13:59:52 +0300

    usage calculation function

--- soffos/service/request/gpt3.py
+++ soffos/service/request/gpt3.py
@@ -1,8 +1,19 @@
 from ._base import ServiceRequestSession
+import typing as t
 
 
 class GPT3Service(ServiceRequestSession):
     name = 'soffos-service-gpt3'
+    Usage = t.Dict[str, t.Any]
+
+    def calculate_usage_overview(usages: t.List[Usage]):
+        return {
+            'prompt_tokens': sum(u['prompt_tokens'] for u in usages),
+            'completion_tokens': sum(u['completion_tokens'] for u in usages),
+            'total_tokens': sum(u['total_tokens'] for u in usages),
+            'total_cost_dollars': sum(u['cost_dollars'] for u in usages),
+            'calls': len(usages)
+        }
 
     def generate(
         self,
