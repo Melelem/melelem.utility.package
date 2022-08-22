07aed4d3582323f99c4995d9f91dc628251cec31
andrch-FS <andreas.christofi@fountech.solutions>
2022-08-22 13:53:10 +0300

    added utilities

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import argparse
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
 YEAR, MONTH, DAY = 22, 8, 22  # date
-HOUR, MINUTE, SECOND = 8, 21, 15  # time
+HOUR, MINUTE, SECOND = 10, 52, 58  # time
 
 
 arg_parser = argparse.ArgumentParser()
