a642b053f1da29995c2c53263f25cd9bf65b99a8
andrch-FS <andreas.christofi@fountech.solutions>
2022-09-21 21:35:13 +0300

    coref pronouns parameter

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import argparse
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
 YEAR, MONTH, DAY = 22, 9, 21  # date
-HOUR, MINUTE, SECOND = 17, 37, 29  # time
+HOUR, MINUTE, SECOND = 18, 34, 56  # time
 
 
 arg_parser = argparse.ArgumentParser()
--- soffos/service/request/coref.py
+++ soffos/service/request/coref.py
@@ -4,9 +4,12 @@ from typing import List
 class CorefModelService(ServiceRequestSession):
     name = "soffos-service-model-coref"
 
-    def resolve(self, texts: List[str]):
+    def resolve(self, texts: List[str], pronouns: List[str] = None):
 
         json = {"texts": texts}
+
+        if pronouns is not None:
+            json["pronouns"] = pronouns
 
         return self.request(json=json, path="resolve")
         
