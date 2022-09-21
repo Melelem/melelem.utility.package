d8b643103e2c37f29e93ff49242329b8963902e4
andrch-FS <andreas.christofi@fountech.solutions>
2022-09-21 20:27:31 +0300

    fixes

--- soffos/service/request/query_parser.py
+++ soffos/service/request/query_parser.py
@@ -4,5 +4,5 @@ from ._base import ServiceRequestSession
 class QueryParserService(ServiceRequestSession):
     name = "soffos-service-query-parser"
 
-    def parse(self, message: str):
-        return self.request(json={"message": message})
+    def parse(self, query: str):
+        return self.request(json={"query": query})
