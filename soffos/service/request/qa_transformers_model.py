bd43bfdfa18d8dd6f1ba8762633db2d31b654d3f
andrch-FS <andreas.christofi@fountech.solutions>
2022-10-24 16:41:22 +0300

    remove old service

--- soffos/service/request/qa_transformers_model.py
+++
@@ -1,8 +0,0 @@
-from ._base import ServiceRequestSession
-
-
-class QATransformersModelService(ServiceRequestSession):
-    name = 'soffos-service-model-qa-transformers'
-
-    def infer(self, context: str, question: str):
-        return self.request({'context': context, 'question': question})
