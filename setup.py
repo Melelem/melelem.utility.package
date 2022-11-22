0d9f94fe7ee8f2a2ce5a615d5cd4aca6283808b2
andrch-FS <andreas.christofi@fountech.solutions>
2022-11-22 19:03:44 +0200

    ner model inputs

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import argparse
 # Set the version to be the 24hr, UTC+0 datetime stamp:
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
-YEAR, MONTH, DAY = 22, 11, 21  # date
-HOUR, MINUTE, SECOND = 13, 18, 59  # time
+YEAR, MONTH, DAY = 22, 11, 22  # date
+HOUR, MINUTE, SECOND = 17, 3, 31  # time
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
--- soffos/service/request/ner_model.py
+++ soffos/service/request/ner_model.py
@@ -4,5 +4,13 @@ from ._base import ServiceRequestSession
 class NERModelService(ServiceRequestSession):
     name = 'soffos-service-model-ner-ontonotes'
 
-    def infer(self, text: str):
-        return self.request({'text': text})
+    def infer(self, text: str = None, texts: list[str] = None):
+
+        json = {}
+
+        if text is not None:
+            json['text'] = text
+        if texts is not None:
+            json['texts'] = texts
+
+        return self.request(json=json)
