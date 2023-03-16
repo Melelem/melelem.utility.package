3f704188e0a0c2dd2ecdbb88607940f5b51b469b
andrch-FS <andreas.christofi@fountech.solutions>
2023-03-16 17:35:16 +0200

    renaming service

---
+++ soffos/service/request/openai.py
@@ -0,0 +1,117 @@
+from ._base import ServiceRequestSession
+import typing as t
+
+
+class OpenAIServcice(ServiceRequestSession):
+    name = 'soffos-service-openai'
+
+    def complete(
+        self,
+        prompt: t.Union[str, list],
+        max_tokens: int = None,
+        stop: str = None,
+        api_key: str = None,
+        engine: str = None,
+        temperature: float = None,
+        top_p: float = None,
+        frequency_penalty: float = None,
+        presence_penalty: float = None,
+        logprobs: int = None,
+        validate_prompt_content: bool = None,
+        user: str = None
+    ):
+        """The generate endpoint calls the "Complete" engine of OpenAI.
+
+        Args:
+            prompt (str, list): Few-shot example prompt. Or list of prompts. If list is provided, the result will contain multiple outputs.
+            stop (str): Stop sequence. Once this sequence of characters is generated, the model stops generating more text.
+            max_tokens (int): Maximum allowed tokens to generate. Make sure this value added with the prompt length does not exceed the maxiumum input length of the model. Service default is 200.
+            api_key (str): API key to be used when calling OpenAI. This should belong to the client whose end-user is making the call.
+            engine (str, optional): Which model to use. Defaults to None. Service default is "text-curie-001". Valid options: "text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001".
+            temperature (float, optional): The level of "creativity". Defaults to None. Service default is 0.0.
+            top_p (float, optional): Controls diversity via nucleus sampling. 0.5 means half of all likelihood-weighted options are considered. Defaults to None. Service default is 1.0.
+            frequency_penalty (float, optional): How much to penalize new tokens based on their frequency so far. Decreases the model's likelihood to repeat the same line verbatim. Defaults to None. Service default is 0.0.
+            presence_penalty (float, optional): How much to penalize new tokens based on whether they appear in the text so far. Increases the model's likelihood to talk about new topics. Defaults to None. Service defailt is 0.0.
+            validate_prompt_content (bool, optional): Whether to check if the content is sensitive or offensive. Defaults to None.
+
+        Returns:
+            _type_: OpenAIObject augmented with additional metadata such as costings.
+        """
+
+        json = {'prompt': prompt}
+
+        if max_tokens is not None:
+            json["max_tokens"] = max_tokens
+        if stop is not None:
+            json["stop"] = stop
+        if api_key is not None:
+            json["api_key"] = api_key
+        if engine is not None:
+            json["engine"] = engine
+        if temperature is not None:
+            json["temperature"] = temperature
+        if top_p is not None:
+            json["top_p"] = top_p
+        if frequency_penalty is not None:
+            json["frequency_penalty"] = frequency_penalty
+        if presence_penalty is not None:
+            json["presence_penalty"] = presence_penalty
+        if logprobs is not None:
+            json["logprobs"] = logprobs
+        if validate_prompt_content is not None:
+            json["validate_prompt_content"] = validate_prompt_content
+        if user is not None:
+            json['user'] = user
+
+        return self.request(json=json, path="complete")
+    
+
+    def chat(
+        self,
+        messages: list[dict[str, str]],
+        max_tokens: int = None,
+        stop: str = None,
+        api_key: str = None,
+        engine: str = None,
+        temperature: float = None,
+        top_p: float = None,
+        n: int = None,
+        frequency_penalty: float = None,
+        presence_penalty: float = None,
+        logit_bias: dict = None,
+        validate_prompt_content: bool = None,
+        user: str = None
+    ):
+        
+        json = {"messages": messages}
+
+        if max_tokens is not None:
+            json["max_tokens"] = max_tokens
+        if stop is not None:
+            json["stop"] = stop
+        if api_key is not None:
+            json["api_key"] = api_key
+        if engine is not None:
+            json["engine"] = engine
+        if temperature is not None:
+            json["temperature"] = temperature
+        if top_p is not None:
+            json["top_p"] = top_p
+        if n is not None:
+            json["n"] = n
+        if frequency_penalty is not None:
+            json["frequency_penalty"] = frequency_penalty
+        if presence_penalty is not None:
+            json["presence_penalty"] = presence_penalty
+        if logit_bias is not None:
+            json["logit_bias"] = logit_bias
+        if validate_prompt_content is not None:
+            json["validate_prompt_content"] = validate_prompt_content
+        if user is not None:
+            json["user"] = user
+
+        return self.request(json=json, path="chat")
+
+
+    def count_tokens(self, text: str):
+        return self.request(json={'text': text}, path='count-tokens')
---
+++ soffos/utilities/openai.py
@@ -0,0 +1,128 @@
+from enum import Enum
+import tiktoken
+import typing as t
+import re
+
+class Tokenizer:
+    def __init__(self) -> None:
+        self.tokenizers = {}
+    def __call__(self, engine):
+        if engine not in self.tokenizers:
+            self.tokenizers[engine] = tiktoken.encoding_for_model(engine)
+        return self.tokenizers[engine]
+
+TOKENIZER = Tokenizer()
+
+class GPTEngine(str, Enum):
+    gpt4_8k = 'gpt-4'
+    gpt4_32k = 'gpt-4-32k' 
+    chatgpt = 'gpt-3.5-turbo'
+    davinci = 'text-davinci-003'
+    curie = 'text-curie-001'
+    babbage = 'text-babbage-001'
+    ada = 'text-ada-001'
+
+class GPTEngineSpecifications(t.NamedTuple):
+    max_tokens: int
+
+GPT_ENGINE_SPECS = {
+    GPTEngine.gpt4_8k: GPTEngineSpecifications(
+        max_tokens=8192
+    ),
+    GPTEngine.gpt4_32k: GPTEngineSpecifications(
+        max_tokens=32768
+    ),
+    GPTEngine.chatgpt: GPTEngineSpecifications(
+        max_tokens=4096
+    ),
+    GPTEngine.davinci: GPTEngineSpecifications(
+        max_tokens=4000
+    ),
+    GPTEngine.curie: GPTEngineSpecifications(
+        max_tokens=2048
+    ),
+    GPTEngine.babbage: GPTEngineSpecifications(
+        max_tokens=2048
+    ),
+    GPTEngine.ada: GPTEngineSpecifications(
+        max_tokens=2048
+    )
+}
+
+class Prompt:
+    def __init__(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as prompt_file:
+            self.text = prompt_file.read()
+        self.char_count = len(self.text)
+        self.word_count = len(self.text.split())
+    
+    def format_text(self, replacements: t.Dict[str, str]):
+        """
+        Replaces the placeholders in the prompt with the required information.
+
+        Args:
+            replacements (t.Dict[str, str]): Dictionary with string literals to be replaced as keys
+            and the replacement strings as values.
+
+        Raises:
+            ValueError: If the literal string is not following the convention.
+            ValueError: If the literal string has been found more than once in the text.
+
+        Returns:
+            _type_: Propmpt's raw text with the replacements.
+        """
+        text = self.text
+        for literal, replacement in replacements.items():
+            if not literal.startswith("{__") or not literal.endswith("__}"):
+                example = "{__replace__}"
+                raise ValueError(f"The literal string to be replaced \"{literal}\" is not following the required convention. Please provide a string enclosed with curlies and double underscores, for example: \"{example}\"")
+            if len([m for m in re.finditer(literal, text)]) > 1:
+                raise ValueError(f"The literal string to be replaced \"{literal}\" has been found multiple times in the prompt. Please format the prompt with unique placeholders.")
+            text = text.replace(literal, replacement)
+
+        return text
+Usage = t.Dict[str, t.Any]
+
+
+def calculate_usage_overview(usages: t.List[Usage]):
+    return {
+        'prompt_tokens': sum(u['prompt_tokens'] for u in usages),
+        'completion_tokens': sum(u['completion_tokens'] for u in usages),
+        'total_tokens': sum(u['total_tokens'] for u in usages),
+        'total_cost_dollars': sum(u['cost_dollars'] for u in usages),
+        'calls': len(usages)
+    }
+
+
+def calculate_max_allowed_tokens(
+    engine: GPTEngine,
+    prompt: str
+):
+    """Calculates how many tokens are remaining for completion given the model size and prompt.
+
+    Args:
+        engine (GPTEngine): The engine.
+        prompt (str): The prompt.
+    """
+
+    return GPT_ENGINE_SPECS[engine].max_tokens - len(TOKENIZER(engine.value).encode(prompt))
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
+        num_tokens = 0
+        for message in messages:
+            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":  # if there's a name, the role is omitted
+                    num_tokens += -1  # role is always required and always 1 token
+        num_tokens += 2  # every reply is primed with <im_start>assistant
+        return num_tokens
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
