de906487cce0297efcef49b8c1adcaebde789ee1
andrch-FS <andreas.christofi@fountech.solutions>
2023-04-07 16:01:31 +0300

    openai updates

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import argparse
 # Set the version to be the 24hr, UTC+0 datetime stamp:
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
-YEAR, MONTH, DAY = 23, 3, 17  # date
-HOUR, MINUTE, SECOND = 9, 14, 7  # time
+YEAR, MONTH, DAY = 23, 4, 7  # date
+HOUR, MINUTE, SECOND = 13, 1, 2  # time
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
