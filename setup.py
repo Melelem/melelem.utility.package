c224783af98b09f208137f7b5dd40ed08c95d74a
andrch-FS <andreas.christofi@fountech.solutions>
2023-05-19 12:08:10 +0300

    update

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import argparse
 # Set the version to be the 24hr, UTC+0 datetime stamp:
 #   1. by hand (see: https://www.utctime.net/).
 #   2. run this script with version arg ('python setup.py -v').
-YEAR, MONTH, DAY = 23, 5, 17  # date
-HOUR, MINUTE, SECOND = 15, 44, 26  # time
+YEAR, MONTH, DAY = 23, 5, 19  # date
+HOUR, MINUTE, SECOND = 9, 5, 36  # time
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
