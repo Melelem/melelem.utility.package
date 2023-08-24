dceca1ee3fba17e7b0f770ae436228b7f83cfd32
Andreas <andreas.christofi@live.com>
2023-08-24 11:28:05 +0300

    deleted imports

--- soffos/service/request/__init__.py
+++ soffos/service/request/__init__.py
@@ -1,9 +1,5 @@
-from .answer_extraction import AnswerExtractionService
-from .embeddings_model import EmbeddingsModelService
 from .coref import CorefModelService
 from .documents import DocumentsService
-from .gpt3 import GPT3Service
-from .ner_model import NERModelService
 from .openai import OpenAIService
 from .paraphrase import ParaphraseService
 from .profanity_model import ProfanityModelService
@@ -14,6 +10,5 @@ from .question_answering import QuestionAnsweringService
 from .stanza import StanzaService
 from .string_similarity import StringSimilarityService
 from .summarization import SummarizationService
-from .tag import TagService
 from .transformer_models import TransformerModelService
 from .translation import TranslationService
